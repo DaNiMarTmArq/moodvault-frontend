8ee5977ff4ddefe5c30ae8d819e56008af2a679c
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-01-18 12:10:15 +0100

    Global theme colors configured

--- moodvault/tailwind.config.js
+++ moodvault/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      primary: "#89ADAD",
+      fill: "#F0F2F2",
+      gray: "#717171",
+    },
     extend: {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
