487f8f4a37c71587a2492e7a4ad7284af26b0fa5
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-01-26 15:22:51 +0100

    Feature/Auth: Login method

--- moodvault/src/app/features/auth/services/AuthService.ts
+++ moodvault/src/app/features/auth/services/AuthService.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { User } from '../../../shared/models/User';
 import { AuthResponse } from '../models/AuthResponse';
 import { IAuthService } from './IAuthService';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,46 +15,21 @@ export class AuthService implements IAuthService {
   private http = inject(HttpClient);
   private router = inject(Router);
 
-  login(user: User): void {
-    this.http.post<AuthResponse>(`${this.baseUrl}/login`, user).subscribe({
-      next: (response) => {
-        if (response.token) {
-          localStorage.setItem('jwtToken', response.token);
-          this.router.navigate(['/dashboard']);
-        } else {
-          console.error('Login failed:', response.message);
-          alert(response.message);
-        }
-      },
-      error: (error) => {
-        let errorMessage = error.error.error;
-        console.error('An error occurred during login:', error);
-        alert(
-          'An error occurred during login. Please try again. ' + errorMessage
-        );
-      },
-    });
+  login(user: User): Observable<boolean> {
+    return this.http.post<AuthResponse>(`${this.baseUrl}/login`, user).pipe(
+      map((response) => {
+        if (response.error) return false;
+        localStorage.setItem('token', response.token);
+        return true;
+      }),
+      catchError((error) => {
+        console.error('Login failed', error);
+        return of(false);
+      })
+    );
   }
 
-  register(user: User): void {
-    this.http.post<AuthResponse>(`${this.baseUrl}/register`, user).subscribe({
-      next: (response) => {
-        if (response.token) {
-          // Save the token to localStorage
-          localStorage.setItem('jwtToken', response.token);
-          // Navigate to the homepage
-          this.router.navigate(['/']);
-        } else {
-          // Handle the error message
-          console.error('Registration failed:', response.message);
-          alert(response.message);
-        }
-      },
-      error: (error) => {
-        // Handle HTTP or network errors
-        console.error('An error occurred during registration:', error);
-        alert('An error occurred during registration. Please try again.');
-      },
-    });
+  register(user: User): Observable<boolean> {
+    throw new Error();
   }
 }
--- moodvault/src/app/features/auth/services/IAuthService.ts
+++ moodvault/src/app/features/auth/services/IAuthService.ts
@@ -1,6 +1,7 @@
+import { Observable } from 'rxjs';
 import { User } from '../../../shared/models/User';
 
 export interface IAuthService {
-  login(user: User): void;
-  register(user: User): void;
+  login(user: User): Observable<boolean>;
+  register(user: User): Observable<boolean>;
 }
