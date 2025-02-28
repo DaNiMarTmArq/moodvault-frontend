27d06957098a1f69cb5998b4b82b801d642f1c91
Daniel Martinez <danimartinezmarquez@gmail.com>
2025-02-28 13:48:28 +0100

    Create mood component skeleton

--- moodvault/src/app/features/moods/components/create-mood/create-mood.component.ts
+++ moodvault/src/app/features/moods/components/create-mood/create-mood.component.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { WrapperLayoutComponent } from '../../../../shared/layouts/wrapper-layout/wrapper-layout.component';
 import { RangeInput } from './components/range-input.component';
+import { FormsModule } from '@angular/forms';
+import { CenterLayoutComponent } from '../../../../shared/layouts/center-layout/center-layout.component';
 
 @Component({
   selector: 'app-create-mood',
   standalone: true,
-  imports: [WrapperLayoutComponent, RangeInput],
+  imports: [
+    WrapperLayoutComponent,
+    RangeInput,
+    FormsModule,
+    CenterLayoutComponent,
+  ],
   templateUrl: './create-mood.component.html',
 })
 export class CreateMood {}
--- moodvault/src/app/features/moods/components/dashboard/dashboard-home.component.ts
+++ moodvault/src/app/features/moods/components/dashboard/dashboard-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WrapperLayoutComponent } from '../../../../shared/layouts/wrapper-layout/wrapper-layout.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-moods-list-view',
   standalone: true,
-  imports: [WrapperLayoutComponent],
+  imports: [WrapperLayoutComponent, RouterModule],
   templateUrl: './dashboard-home.component.html',
 })
 export class MoodsDashboard {}
